245e66d8af0341fc829872344a618d5baef8330a
NewbiePro <2383779481@qq.com>
2025-02-05 00:18:00 +0800

    git init

---
+++ quantax_lab_common/quantax_lab_common_cache/src/main/java/com/quantax/lab/common/cache/QuantaxLabCommonCacheApplication.java
@@ -0,0 +1,13 @@
+package com.quantax.lab.common.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantaxLabCommonCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuantaxLabCommonCacheApplication.class, args);
+    }
+
+}
---
+++ quantax_lab_common/quantax_lab_common_utils/src/test/java/com/quantax/lab/common/utils/QuantaxLabCommonUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantax.lab.common.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantaxLabCommonUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
